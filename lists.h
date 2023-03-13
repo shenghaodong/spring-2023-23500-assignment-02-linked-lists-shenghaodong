a246510534c6f3a50a2af956b4c6d3da0098adc3
Shenghao Dong <shenghaodong18@gmail.com>
2023-03-12 20:46:13 -0400

    finished lists and node as well as add makefile + test

--- lists.h
+++ lists.h
@@ -3,11 +3,16 @@
 #include "Node.h"
 
 class List{
-    private:
-        Node *head;
+ private:
+  Node *head;
+ public:
+  List();
+  ~List();
+  void insert(std::string data);
+  void insert(int loc, std::string data); //0 indexed
+  int length();
+  std::string find(std::string data);
 
-    public:
-        List();
-        void insert(std::string data);
-        
-}
+  std::string toString(); // for testing purposes
+  void remove(int loc);
+};
--- node.h
+++ node.h
@@ -1,18 +1,17 @@
 #pragma once
 
 class Node{
-    private:
-    std::string data;
-    Node *next;
+ private:
+  std::string data;
+  Node *next;
+ public:
+  Node();
+  Node(std::string data);
+  Node(std::string data, Node *next);
 
-    public:
-    Node();
-    Node(std::string data);
-    Node(std::string data, Node *next);
+  void setData(std::string data);
+  void setNext(Node *next);
 
-    void setData(std::string data);
-    void setNext(Node *next);
-
-    std::string getData();
-    Node *getNext();
-}
+  std::string getData();
+  Node *getNext();
+};
